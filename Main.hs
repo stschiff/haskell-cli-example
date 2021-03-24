fa8bfa2de29c7241a1a4a53b5e07e619b7c83477
Stephan Schiffels <stephan_schiffels@mac.com>
2021-03-24 10:53:43 +0100

    some updates to make the help message nicer

--- Main.hs
+++ Main.hs
@@ -15,29 +15,33 @@ data FormatSpec = PlinkFormat FilePath FilePath FilePath | VCFFormat FilePath de
 data IndividualsSpec = IndividualsByFile FilePath | IndividualsByList [String] deriving (Show)
 
 missingnessParser :: Parser Double
-missingnessParser = option auto (long "missingness" <> short 'm')
+missingnessParser = option auto (long "missingness" <>
+  short 'm' <> help "A missingness threshold" <> value 0.5 <> showDefault <> metavar "NUMBER")
 
 verboseParser :: Parser Bool
-verboseParser = switch (long "verbose" <> short 'v')
+verboseParser = switch (long "verbose" <> short 'v' <> help "verbose output")
 
 summaryStatParser :: Parser SummaryStatSpec
 summaryStatParser = hetParser <|> segSitesParser <|> hwParser
   where
-    hetParser      = flag' Heterozygosity (long "heterozygosity")
-    segSitesParser = flag' SegregatingSites (long "segregatingSites")
-    hwParser       = flag' HardyWeinbergDeviation (long "hardyWeinbergDev")
+    hetParser = flag' Heterozygosity (long "heterozygosity" <>
+      help "compute the rate of heterozygosity for each individual")
+    segSitesParser = flag' SegregatingSites (long "segregatingSites" <>
+      help "compute the rate of segregating sites for each individual")
+    hwParser = flag' HardyWeinbergDeviation (long "hardyWeinbergDev" <>
+      help "compute the average deviation from Hardy-Weinberg equilibrium for each individual")
 
 plinkFormatParser :: Parser FormatSpec
 plinkFormatParser = PlinkFormat <$> genoParser <*> snpParser <*> indParser
   where
-    genoParser = strOption (long "genoFile")
-    snpParser  = strOption (long "snpFile")
-    indParser  = strOption (long "indFile")
+    genoParser = strOption (long "genoFile" <> help "the input genotype file" <> metavar "FILE")
+    snpParser  = strOption (long "snpFile" <> help "the input snp file" <> metavar "FILE")
+    indParser  = strOption (long "indFile" <> help "the input individual file" <> metavar "FILE")
 
 vcfFormatParser :: Parser FormatSpec
 vcfFormatParser = VCFFormat <$> vcfFileParser
   where
-    vcfFileParser = strOption (long "vcfFile")
+    vcfFileParser = strOption (long "vcfFile" <> help "the input VCF file" <> metavar "FILE")
 
 formatParser :: Parser FormatSpec
 formatParser = plinkFormatParser <|> vcfFormatParser
@@ -45,8 +49,11 @@ formatParser = plinkFormatParser <|> vcfFormatParser
 individualsParser :: Parser IndividualsSpec
 individualsParser = individualsFileParser <|> individualsListParser
   where
-    individualsFileParser = IndividualsByFile <$> strOption (long "individualsFile")
-    individualsListParser = IndividualsByList <$> some (strOption (long "ind"))
+    individualsFileParser = IndividualsByFile <$> strOption (long "individualsFile" <>
+      help "list individuals in the file given" <> metavar "FILE")
+    individualsListParser = IndividualsByList <$> some (strOption (long "ind" <>
+      help "list individuals directly on the command line. Option can be given multiple times, once for each individual" <>
+      metavar "NAME"))
 
 optionsParser :: Parser Options
 optionsParser = Options <$> missingnessParser <*> verboseParser <*> summaryStatParser <*> formatParser <*> individualsParser
@@ -58,5 +65,5 @@ main = do
   where
     opts = info (optionsParser <**> helper)
       ( fullDesc
-     <> progDesc "Print a greeting for TARGET"
-     <> header "hello - a test for optparse-applicative" )
+     <> progDesc "Hello, this is a toy example for how to design command line interfaces in Haskell"
+     <> header "Haskell-CLI-Example" )
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # haskell-cli-example
+
+This package contains the source code for my [blog post about command line interface design in Haskell](https://www.stephanschiffels.de/posts/2021-03-23-Haskell-CLI/).
+
+If you want to compile this example, first install [stack](https://docs.haskellstack.org).
+Then clone or download this repository and run `stack run -- --help` from inside. You should then find the help output for this command line interface
+
+```
+```
+Usage: haskell-cli-example-exe [-m|--missingness NUMBER] [-v|--verbose] 
+                               (--heterozygosity | --segregatingSites | 
+                                 --hardyWeinbergDev) 
+                               (--genoFile FILE --snpFile FILE --indFile FILE | 
+                                 --vcfFile FILE) 
+                               (--individualsFile FILE | --ind NAME)
+  Hello, this is a toy example for how to design command line interfaces in
+  Haskell
+
+Available options:
+  -m,--missingness NUMBER  A missingness threshold (default: 0.5)
+  -v,--verbose             verbose output
+  --heterozygosity         compute the rate of heterozygosity for each
+                           individual
+  --segregatingSites       compute the rate of segregating sites for each
+                           individual
+  --hardyWeinbergDev       compute the average deviation from Hardy-Weinberg
+                           equilibrium for each individual
+  --genoFile FILE          the input genotype file
+  --snpFile FILE           the input snp file
+  --indFile FILE           the input individual file
+  --vcfFile FILE           the input VCF file
+  --individualsFile FILE   list individuals in the file given
+  --ind NAME               list individuals directly on the command line. Option
+                           can be given multiple times, once for each individual
+  -h,--help                Show this help text
+```
